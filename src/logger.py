b2f4358f023061ecc752f909998e5d6b1e54feb6
Zaili Gu <zoelingu0731@gmail.com>
2025-09-28 20:32:38 -0400

    Made little changes

---
+++ src/logger.py
@@ -0,0 +1,5 @@
+
+# IMPORTANT: no logging calls here (so importing this module won't write lines)
+# if __name__ == "__main__":   # optional manual test
+#     logging.info("Logging has started")
+#     print(f"Log: {LOG_FILE_PATH}")
